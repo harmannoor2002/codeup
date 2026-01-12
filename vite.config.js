3ed52cc1f31dcdd6e8724ce161559e440de75764
Harman Noor <harmannoor2002@gmail.com>
2026-01-12 05:32:56 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "public",   // <-- tells Vite to use public/ as root
   plugins: [react()],
-  server: {
-    port: 5173
-  }
+  build: {
+    outDir: "../dist", // output dist folder at project root
+  },
+  base: "/",        // ensures correct paths
 });

5b8826c0171bf988d16341a19acf009599845251
Harman Noor <harmannoor2002@gmail.com>
2026-01-14 05:28:24 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="container">
-      <h1>🚀 Auto Deployment Successful through codeupnow</h1>
+      <h1>🚀 Auto Deployment Successful through codeupnoww</h1>
       <p>
         This React app is deployed automatically using:
       </p>
